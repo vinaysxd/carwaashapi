a00cd01b5c29fd915985c85d322ded2f1ab6841e
vinaysxd <vinaysounderraj5@gmail.com>
2026-05-23 17:55:48 +0530

    feat: supabase client connecction setup

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,1 +1,18 @@
-module.exports = {};
+require('dotenv').config();
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_PROJECT_URL;
+console.log(supabaseUrl)
+const supabaseServiceRoleKey = process.env.SUPA_BASE_SERVICE_ROLE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
+
+module.exports = supabase;
+
+if (require.main === module) {
+  if (supabase && supabase.auth) {
+    console.log('Supabase client initialized successfully');
+  } else {
+    console.error('Supabase connection failed');
+  }
+}
