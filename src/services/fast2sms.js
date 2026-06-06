61a76c6a646dd967dfe6e01830077a6cabfde450
vinaysxd <vinaysounderraj5@gmail.com>
2026-06-06 21:11:33 +0530

    Rate limiter fix: final change attempts to 3 for prod

--- src/services/fast2sms.js
+++ src/services/fast2sms.js
@@ -6,30 +6,7 @@ const FAST2SMS_SENDER_ID = process.env.FAST2SMS_SENDER_ID;
 const FAST2SMS_TEMPLATE_ID = process.env.FAST2SMS_TEMPLATE_ID;
 const FAST2SMS_MESSAGE_ID = process.env.FAST2SMS_MESSAGE_ID
 
-async function sendOTP(phoneNumber, otp) {
-//   console.log(`=>Your OTP is ${otp}. Valid for 5 minutes. Do not share this code with anyone. - Carwaash`)
-//   try {
-// var params =  {
-//     authorization: FAST2SMS_API_KEY,
-//     sender_id: FAST2SMS_SENDER_ID,
-//     message: FAST2SMS_TEMPLATE_ID,
-//     variables_values: `${otp}|5|Carwaash`,
-//     route: 'dlt',
-//     numbers: phoneNumber,
-//   }
-//   console.log(params)
-//     const response = await axios({
-//   method: 'GET',
-//   url: 'https://www.fast2sms.com/dev/bulkV2',
-//   params: params
-// });
-
-    
-//     return response.data;
-//   } catch (err) { 
-//     console.error(err.response ? err.response.data : err.message);
-//     throw err;
-//   }
+async function sendOTP(phoneNumber, otp) { 
 var url = `https://www.fast2sms.com/dev/bulkV2?authorization=${FAST2SMS_API_KEY}&sender_id=${FAST2SMS_SENDER_ID}&message=${FAST2SMS_MESSAGE_ID}&variables_values=${otp}%7C10%7CCarwaash&route=dlt&numbers=${phoneNumber}`
 console.log("URL: ",url)
 const options = {
